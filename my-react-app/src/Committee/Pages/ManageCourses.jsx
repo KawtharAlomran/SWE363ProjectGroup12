428c412cd17dd679dcaf7ec460cbaeebc0c81cc9
KawtharAlomran <s202275440@kfupm.edu.sa>
2026-04-05 21:00:03 +0300

    Add main structure for ManageCourses page

--- my-react-app/src/Committee/Pages/ManageCourses.jsx
+++ my-react-app/src/Committee/Pages/ManageCourses.jsx
@@ -1,3 +1,45 @@
+import { useState } from "react";
+
+const coursesList = [
+    { code: "ICS 104", name: "Intro. to Prog. in Python & C" },
+    { code: "ICS 108", name: "Object-Oriented Programming" },
+    { code: "ICS 202", name: "Data Structures and Algorithms" },
+    { code: "ICS 253", name: "Discrete Structures" },
+    { code: "ICS 321", name: "Database Systems" },
+    { code: "ICS 343", name: "Fund. of Computer Networks" },
+    { code: "ICS 344", name: "Information Security" },
+    { code: "ICS 353", name: "Design/Analysis of Algorithms" },
+    { code: "ICS 381", name: "Principles of Artificial Intelligence"},
+    { code: "ICS 410", name: "Programming Languages"}
+  ];
+
+
+function renderCourses(courses, handleDelete) {
+  return courses.map((course) => (
+    <div key={course.code}>
+      <span>{course.code}</span>
+      <span>{course.name}</span>
+      <button onClick={() => handleDelete(course.code)}>Delete</button>
+    </div>
+  ));
+}
+
 export default function ManageCourses() {
-  return <h2>Manage Courses</h2>;
+  const [courses, setCourses] = useState(coursesList);
+  const handleDelete = (code) => {
+      setCourses((prevCourses) => prevCourses.filter((course) => course.code !== code));
+  };
+return (
+    <div>
+      <div>
+        <h2>Manage Courses</h2>
+        <button>Add new Course</button>
+      </div>
+      <div>
+        <div>Course number</div>
+        <div className="text-center">Course Name</div>
+      </div>
+      {renderCourses(courses, handleDelete)}
+    </div>
+  )
 }
