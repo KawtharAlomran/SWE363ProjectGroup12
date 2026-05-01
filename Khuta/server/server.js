2583c7383afa5474db80b03ad57832a9ba2f9b95
FatimahTawfiq <s202272340@kfupm.edu.sa>
2026-05-01 12:02:15 +0300

    create database connection

---
+++ Khuta/server/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+
+import dotenv  from "dotenv";
+import { connectDB } from "./db.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5174;
+
+app.use(cors());              
+app.use(express.json());
+
+await connectDB(process.env.MONGO_URL);
+
+app.listen(PORT, () => console.log(`API running on http://localhost:${PORT}`));
