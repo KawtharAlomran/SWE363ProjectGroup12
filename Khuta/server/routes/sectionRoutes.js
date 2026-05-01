6532a0f995d560eb96f791c963c190e0d446286b
Nour-AlSulais <s202271740@kfupm.edu.sa>
2026-05-01 19:10:07 +0300

    conect with sections data

--- Khuta/server/routes/sectionRoutes.js
+++ Khuta/server/routes/sectionRoutes.js
@@ -1,6 +1,5 @@
 import express from "express";
-import { addSections } from "../controllers/sectionController.js";
- 
+import { addSections } from "../controllers/termSectionController.js"; 
 const router = express.Router();
  
 // POST — save sections and term after submit
