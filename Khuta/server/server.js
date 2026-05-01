3b3c50cff4ef3b5157c621ce5c1d04b19cf0c13d
Nour-AlSulais <s202271740@kfupm.edu.sa>
2026-05-02 01:37:12 +0300

    backend assigment data

--- Khuta/server/server.js
+++ Khuta/server/server.js
@@ -14,6 +14,7 @@ import planRoutes from "./routes/planRoutes.js";
 import sectionRoutes from "./routes/sectionRoutes.js";
 import preferenceRoutes from "./routes/preferenceRoutes.js";
 import teachingLoadRoutes from "./routes/loadRoute.js";
+import assignmentRoutes from "./routes/assignmentRoutes.js";
 
 
 
@@ -29,6 +30,7 @@ app.use("/api/plans", planRoutes);
 app.use("/api/sections", sectionRoutes);
 app.use("/api/preferences", preferenceRoutes);
 app.use("/api/assignments/load", teachingLoadRoutes);
+app.use("/api/assignments", assignmentRoutes);
 
 await connectDB(process.env.MONGO_URL);
 
