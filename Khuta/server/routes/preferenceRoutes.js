f23d3786fe79c1c6416695c5ad80244b5f872da1
Lama-cmd1 <s202275940@kfupm.edu.sa>
2026-05-02 16:38:21 +0300

    fix upcoming term logic and enable preference submission

--- Khuta/server/routes/preferenceRoutes.js
+++ Khuta/server/routes/preferenceRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import { getPreferencesByTerm } from "../controllers/preferenceController.js";
 import { getPreferencesByInstructor } from "../controllers/preferenceController.js";
 import { getPreferencesByCourse } from "../controllers/preferenceController.js";
+import { submitPreferences } from "../controllers/preferenceController.js";
 
 const router = express.Router();
 
@@ -11,5 +12,7 @@ router.get("/term/:termId", getPreferencesByTerm);
 router.get("/term/:termId/instructor", getPreferencesByInstructor);
 // return preferences grouped by course (used in "By Course" view)
 router.get("/term/:termId/course", getPreferencesByCourse);
+// post submitted preferences (used in SetPreferences.jsx)
+router.post("/", submitPreferences);
 
 export default router;
